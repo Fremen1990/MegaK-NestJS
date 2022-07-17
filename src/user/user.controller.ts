38cc895ba0d689b2c6c2b0d2f3d170b7c903d4cb
DevThomas <fremen1990@gmail.com>
2022-07-17 21:52:13 +0200

    DAY-16  Register user implemented

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Inject, Post} from '@nestjs/common';
+import {RegisterUserResponse} from "../interfaces/user";
+import {UserService} from "./user.service";
+import {RegisterDto} from "./dto/register.dto";
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+
+    constructor(
+        @Inject(UserService) private userService: UserService
+    ) {
+    }
+
+
+@Post('/register')
+    register(
+        @Body() newUser: RegisterDto
+): Promise<RegisterUserResponse>{
+return this.userService.register(newUser)
+}
+
+
+
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import {User} from "./user.entity";
+import {RegisterDto} from "./dto/register.dto";
+import {RegisterUserResponse} from "../interfaces/user";
+
+@Injectable()
+export class UserService {
+
+   async  register(newUser: RegisterDto): Promise<RegisterUserResponse> {
+    const user = new User();
+    user.email = newUser.email
+       await user.save()
+
+       return user;
+    }
+}
