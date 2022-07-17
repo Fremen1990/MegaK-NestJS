2f71980a7c57316ec222428d5f6a3825ae735944
DevThomas <fremen1990@gmail.com>
2022-07-17 21:13:21 +0200

    DAY-16  init module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,12 @@ import {ShopService} from './shop/shop.service';
 import {BasketController} from './basket/basket.controller';
 import {BasketService} from './basket/basket.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
+import { UserModule } from './user/user.module';
 
 @Module({
     imports: [
         TypeOrmModule.forRoot(),
+        UserModule,
     ],
     controllers: [AppController, ShopController, BasketController],
     providers: [AppService, ShopService, BasketService],
